e04191934c0e2d08111bc61c1c758fff962e9e49
marshalltaylorSFE <marshall.taylor@sparkfun.com>
2017-09-18 18:40:51 -0600

    Finished proto v01 test doc

---
+++ Software/Serial2LoopBack/Serial2LoopBack.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+HardwareSerial Serial1(2);  // UART1/Serial1 pins 16,17
+
+#define AUX_LED_PIN 13
+
+
+void setup(){
+	pinMode(AUX_LED_PIN, OUTPUT);
+
+    Serial.begin(115200);
+	Serial1.begin(115200);
+	
+	delay(1000);
+	Serial.println("Starting Sketch");
+	digitalWrite(AUX_LED_PIN, 1);
+	
+}
+int toggler = 0;
+
+void loop(){
+	char c;
+	if(Serial.available())
+	{
+		c = Serial.read();
+		Serial1.print(c);
+		digitalWrite(AUX_LED_PIN, toggler);
+		toggler ^= 0x01;
+	}
+	if(Serial1.available())
+	{
+		c = Serial1.read();
+		Serial.print(c);
+	}
+	delay(10);
+}
