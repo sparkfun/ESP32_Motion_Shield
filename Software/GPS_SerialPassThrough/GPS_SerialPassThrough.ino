62aa29aa3beaf4f51614558fd8fe23acd221889f
Evorock <evorock@alswblog.org>
2018-09-13 01:10:52 -0600

    Update GPS_SerialPassThrough.ino
    Modify the variable " HardwareSerial Serial2(2); " because it does not compile, change the vairbale to " HardwareSerial GPS(2); "

--- Software/GPS_SerialPassThrough/GPS_SerialPassThrough.ino
+++ Software/GPS_SerialPassThrough/GPS_SerialPassThrough.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
-HardwareSerial Serial1(2);  // UART1/Serial1 pins 16,17
+HardwareSerial GPS(2);  // UART1/Serial1 pins 16,17
 
 void setup()
 {
   Serial.begin(115200);
-  Serial1.begin(9600);
+  GPS.begin(9600);
   delay(1000);
   Serial.println("Sketch Started.");
 }
@@ -13,11 +13,11 @@ void loop() {
   //Pass usb data to the gps
   if (Serial.available())
   {
-    Serial1.write(Serial.read());
+    GPS.write(Serial.read());
   }
   //Pass gps data to the usb
-  if (Serial1.available())
+  if (GPS.available())
   {
-    Serial.write(Serial1.read());
+    Serial.write(GPS.read());
   }
 }
